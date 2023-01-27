c699c19e2729ab2bc6a3ca9a1c6993166ddb1d6c
Chris <carechiga43@gmail.com>
2023-01-27 12:58:28 -0800

    Updated README, added my own comments to code removed comments provided by startet code

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Week-Five-Challenge
 Week five challenge for UCI bootcamp, scheduler
+## Project Description
+The objective for this project was to create a work day planner/schdeuler.
+THe planner needs to display hour-blocks for each hour of the work day.
+In each hour block the user should be able to write information needed for planning the day and save it by clicking the button to the right of the text area
+The saved data will persist in local storage until overwritten and will load automatically when the page is visited
+For ease the planners colors will update as time passes to indicate whether that hour has already passed, it is currently that hour, or the hour is still yet to come.
+
+![alt text](assets/images/Screenshot.png)
+
+## Credits
+
+For this project I was given starter code from our class repository which contained fully functional index.html and style.css documents. These documents were updated as needed to provide functionality.
+
+## Deployemnt
+
+You can find the deployed application here:
+https://carechiga.github.io/Week-Five-Challenge/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,4 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// using Jquery to travers the DOM
 var displayDate = $('#currentDay');
 var timeBlock8 = $('#hour-8');
 var timeBlock9 = $('#hour-9');
@@ -15,32 +13,9 @@ var timeBlock5 = $('#hour-5');
 var currentDate = moment();
 var currentHour = currentDate.format('HH');
 
-
-
-
-
-$(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use thstorage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be use id in the containing time-block as a key to save the user input in
-    // local ed to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  });
-  
+//   this function adds and removes classes to the timeblocks to indicate whether the hour has passed or is yet to come
  function pastPresentFuture() {
+    // this if statement compates the data-hour attribute value to the current times hour
     $('.time-block').each(function() {
     if ( $(this).attr('data-hour') < currentHour) {
         $(this).removeClass('future');
@@ -61,25 +36,27 @@ $(function () {
         };
  });
 }
-
+// this function takes the info from the specific save buttons associated div and stores information in local storage
  function handleSave(event){
     var timeBlockText = $(this).siblings('textarea').val();
     var timeBlockKey = $(this).parent().attr('id');
     localStorage.setItem(timeBlockKey, timeBlockText);
  }
-
+//this function runs through a list of all timeblocks and displays the text that was saved in its associated local storage target
  function displaySave(){
     var arr = ['hour-8', 'hour-9', 'hour-10', 'hour-11', 'hour-12', 'hour-1', 'hour-2', 'hour-3', 'hour-4', 'hour-5'];
     for(var i = 0; i < arr.length; i++){;
+        // since the timeblock appear in order in the DOM I can travers down the parent container #overallContainer and target each timeblock to fill with the associated keyitem in local storage
     $('#overallContainer').children().eq(i).children('textarea').text(localStorage.getItem(arr[i]));}
  }
 
+ //this function displays the time at the top of the page
  function projectDate(){
     displayDate.text('It is ' + moment().format('dddd MMM D, YYYY h:mm:ss A'));
  }
 
  setInterval(projectDate, 100);
- pastPresentFuture();
+ setInterval(pastPresentFuture, 100);
  displaySave();
 
  timeBlock8.children('button').on('click', handleSave);
